428671f66ac03117739e1a78de21d19b9d65fc60
Srujan Dharma <140909375+srujan0404@users.noreply.github.com>
2024-07-05 15:04:32 +0530

    added all files

---
+++ src/utils/constants.js
@@ -0,0 +1,43 @@
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import HomeIcon from "@mui/icons-material/Home";
+import CodeIcon from "@mui/icons-material/Code";
+import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import LiveTvIcon from "@mui/icons-material/LiveTv";
+import SchoolIcon from "@mui/icons-material/School";
+import FaceRetouchingNaturalIcon from "@mui/icons-material/FaceRetouchingNatural";
+import CheckroomIcon from "@mui/icons-material/Checkroom";
+import GraphicEqIcon from "@mui/icons-material/GraphicEq";
+import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
+
+export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
+
+export const categories = [
+  { name: "New", icon: <HomeIcon /> },
+  { name: "Coding", icon: <CodeIcon /> },
+  { name: "ReactJS", icon: <CodeIcon /> },
+  { name: "NextJS", icon: <CodeIcon /> },
+  { name: "Music", icon: <MusicNoteIcon /> },
+  { name: "Education", icon: <SchoolIcon /> },
+  { name: "Podcast", icon: <GraphicEqIcon /> },
+  { name: "Movie", icon: <OndemandVideoIcon /> },
+  { name: "Gaming", icon: <SportsEsportsIcon /> },
+  { name: "Live", icon: <LiveTvIcon /> },
+  { name: "Sport", icon: <FitnessCenterIcon /> },
+  { name: "Fashion", icon: <CheckroomIcon /> },
+  { name: "Beauty", icon: <FaceRetouchingNaturalIcon /> },
+  { name: "Comedy", icon: <TheaterComedyIcon /> },
+  { name: "Gym", icon: <FitnessCenterIcon /> },
+  { name: "Crypto", icon: <DeveloperModeIcon /> },
+];
+
+export const demoThumbnailUrl =
+  "https://www.google.com/url?sa=i&url=https%3A%2F%2Fm.economictimes.com%2Findustry%2Fservices%2Feducation%2Fscaler-launches-scaler-school-of-technology-a-four-year-residential-ug-program-in-computer-science%2Farticleshow%2F99109738.cms&psig=AOvVaw0WD2DzaJe80f2iZO07UqVG&ust=1701075876825000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPCros2n4YIDFQAAAAAdAAAAABAJ";
+export const demoChannelUrl = "/channel/UCZMli3czZnd1uoc1ShTouQw";
+export const demoVideoUrl = "/video/5qap5aO4i9A";
+export const demoChannelTitle = "Srujan Dharma";
+export const demoVideoTitle = "Project Wheel - The evolution of Brain";
+export const demoProfilePicture =
+  "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.scaler.com%2Fschool-of-technology%2Fadmission%2F&psig=AOvVaw0WD2DzaJe80f2iZO07UqVG&ust=1701075876825000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPCros2n4YIDFQAAAAAdAAAAABAR";
